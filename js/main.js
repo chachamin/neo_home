2492c59c42c1cd254f31aa29e5db11fc554e493e
chachamin <wlauswlaus@hanmail.net>
2024-06-20 18:15:04 +0900

    메인 완료

--- js/main.js
+++ js/main.js
@@ -7,13 +7,14 @@ $(document).ready(function(){
 		pauseOnHover: false,
 		speed: 600,
 		arrows: false,
-		autoplaySpeed: 4000,
+		autoplaySpeed: 5000,
 		fade: false,
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		infinite: true,
 		cssEase: 'linear'
 	});
+
     $('.work_sd').slick({
 		autoplay: true,
 		dots: true,
@@ -21,7 +22,7 @@ $(document).ready(function(){
 		pauseOnHover: false,
 		speed: 600,
 		arrows: false,
-		autoplaySpeed: 3000,
+		autoplaySpeed: 5000,
 		fade: false,
 		slidesToShow: 5,
 		slidesToScroll: 1,
@@ -83,6 +84,32 @@ $(document).ready(function(){
 
 	});
 
+	//공동 재생 멈춤
+	slick_stop = $('.slick-stop').on('click', function() {
+		$(this).siblings('.slick-play').css('display','inline-block').focus();
+		$(this).css('display','none');
+		$(this).parent().parent().siblings('.slick-slider').slick('slickPause');
+	});
+
+	$('.slick-play').on('click', function() {
+		$(this).siblings('.slick-stop').css('display','inline-block').focus();
+		$(this).css('display','none');
+		$(this).parent().parent().siblings('.slick-slider').slick('slickPlay');
+	});
+	$('.slick-prev').on('click', function() {
+		$(this).parent().parent().siblings('.slick-slider').slick('slickPrev');
+		$(this).parent().parent().siblings('.slick-slider').slick('slickPause');
+		$(this).siblings('.slick-play').css('display','inline-block');
+		$(this).siblings('.slick-stop').css('display','none');
+	});
+
+	$('.slick-next').on('click', function() {
+		$(this).parent().parent().siblings('.slick-slider').slick('slickNext');
+		$(this).parent().parent().siblings('.slick-slider').slick('slickPause');
+		$(this).siblings('.slick-play').css('display','inline-block');
+		$(this).siblings('.slick-stop').css('display','none');
+	});
+
 	// fixed header(pc)
 	$(window).scroll(function() {
 		if($(window).width() > 0) {
@@ -95,4 +122,80 @@ $(document).ready(function(){
 			}
 		}
 	});
+
+
+	// 메인 - 공지사항 탭
+	$('.board_tabs > li > a.tab_link').on('focus click' ,function(){
+		$(this).addClass('active');		
+		$(this).parent('li').siblings('li').find('a').removeClass('active');		
+		$(this).parent('li').find('div').css('display','block');
+		$(this).parent('li').siblings('li').find('div').css('display','none');
+		$(this).parent('li').find('a.more').css('display','inline-flex');
+		$(this).parent('li').siblings('li').find('a.more').css('display','none');
+		$('.tab_sd').slick("setPosition");
+	});
 });
+
+
+// 메인 - 파트너사 롤링
+$(window).on('load', function() {
+    setFlowBanner();
+})
+
+function setFlowBanner() {
+    const $wrap = $('.flow_banner');
+    const $list = $('.flow_banner .list');
+    let wrapWidth = ''; //$wrap의 가로 크기
+    let listWidth = ''; //$list의 가로 크기
+    const speed = 92; //1초에 몇픽셀 이동하는지 설정
+
+    //리스트 복제
+    let $clone = $list.clone();
+    $wrap.append($clone);
+    flowBannerAct()
+
+    //반응형 :: 디바이스가 변경 될 때마다 배너 롤링 초기화
+    let oldWChk = window.innerWidth > 1279 ? 'pc' : window.innerWidth > 767 ? 'ta' : 'mo';
+    $(window).on('resize', function() {
+        let newWChk = window.innerWidth > 1279 ? 'pc' : window.innerWidth > 767 ? 'ta' : 'mo';
+        if (newWChk != oldWChk) {
+            oldWChk = newWChk;
+            flowBannerAct();
+        }
+    });
+
+    //배너 실행 함수
+    function flowBannerAct() {
+        //배너 롤링 초기화
+        if (wrapWidth != '') {
+            $wrap.find('.list').css({
+                'animation': 'none'
+            });
+            $wrap.find('.list').slice(2).remove();
+        }
+        wrapWidth = $wrap.width();
+        listWidth = $list.width();
+
+        //무한 반복을 위해 리스트를 복제 후 배너에 추가
+        if (listWidth < wrapWidth) {
+            const listCount = Math.ceil(wrapWidth * 2 / listWidth);
+            for (let i = 2; i < listCount; i++) {
+                $clone = $clone.clone();
+                $wrap.append($clone);
+            }
+        }
+        $wrap.find('.list').css({
+            'animation': `${listWidth / speed}s linear infinite flowRolling`
+        });
+    }
+
+    // 마우스가 요소 위로 진입했을 때 일시정지
+    $wrap.on('mouseenter', function () {
+        $wrap.find('.list').css('animation-play-state', 'paused');
+    });
+
+    // 마우스가 요소에서 빠져나갈 때 재생
+    $wrap.on('mouseleave', function () {
+        $wrap.find('.list').css('animation-play-state', 'running');
+    });
+}
